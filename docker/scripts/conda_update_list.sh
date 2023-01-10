915b700b40eb60195f0cd73eaf1bcfc6039e4866
Adrian Grzemski <adrian.grzemski@gmail.com>
2023-01-10 21:03:15 +0100

    Script for generating updated conda manifest

---
+++ docker/scripts/conda_update_list.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -eux
+set -o pipefail
+
+cat <( \
+  conda list --json | \
+  grep -Pvae '[\[\]]'
+) <( \
+  condaup --dry-run --json | \
+  grep -Pzo -e '\"LINK\"\:\s+\[([^\]]+)\]' | \
+  head -n -1 | \
+  tail -n +2 \
+) | \
+grep -v -e '^\/bin\/bash' | \
+awk '
+  BEGIN{
+    RS="\\s+},\\s+";
+    FS = "\n";
+  }
+  {
+      match($7, "\"name\":\\s+\"(\\S+)\"", matched);
+      name = matched[1];
+
+      match($9, "\"version\":\\s+\"(\\S+)\"", matched);
+      version=matched[1];
+
+      print name "=" version;
+      next;
+  }t
+' | \
+awk '
+  BEGIN{
+    RS = "\n";
+    FS = "=";
+  }
+  FNR==NR {
+    registry[$1] = $2
+    next;
+  }
+  /^#/{ print $0; next }
+  !length($0) {print $0; next}
+  {
+    if ($1 in registry){print $1 "=" registry[$1]} else {print $0}
+  }
+' - 'packages/conda.list'
